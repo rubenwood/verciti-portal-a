543b888349b92d98ff5824e2382213887e31dbf3
rubenwood <ruben.wood1@gmail.com>
2025-05-27 12:37:12 +0100

    uri redirect fix

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     const hash = window.location.hash;
     console.log("Hash:", hash);
     if (hash.includes("type=recovery")) {
-      router.replace(`/reset-password${hash}`);
+      window.location.href = `/reset-password${hash}`;
     }
   }, [router]);
 
