2bbb445f829490aed813a7f95202b870d76864e3
rubenwood <ruben.wood1@gmail.com>
2025-05-27 13:25:57 +0100

    improved layout

--- app/update-password/page.tsx
+++ app/update-password/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase";
 
 export default function UpdatePasswordPage() {
   const [password, setPassword] = useState("");
   const [status, setStatus] = useState("");
-  const router = useRouter();
 
   const updatePassword = async () => {
     const { error } = await supabase.auth.updateUser({ password });
@@ -19,19 +17,21 @@ export default function UpdatePasswordPage() {
   };
 
   return (
-    <div className="p-8">
-      <h2 className="text-2xl font-bold mb-4">Reset your password</h2>
-      <input
-        type="password"
-        className="border px-4 py-2 mb-2 w-full"
-        placeholder="New password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button className="button w-full" onClick={updatePassword}>
-        Update Password
-      </button>
-      {status && <p className="mt-2">{status}</p>}
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20">
+        <div>
+            <h2 className="text-2xl font-bold mb-4">Reset your password</h2>
+            <input
+                type="password"
+                className="border px-4 py-2 mb-2 w-full"
+                placeholder="New password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="button w-full" onClick={updatePassword}>
+                Update Password
+            </button>
+            {status && <p className="mt-2">{status}</p>}
+        </div>
     </div>
   );
 }
