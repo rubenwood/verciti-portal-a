4719c42f65dc111548a72cd999c4fe639209f283
rubenwood <ruben.wood1@gmail.com>
2025-06-11 16:54:18 +0100

    borken code

--- app/model-browser/model-browser-component.tsx
+++ app/model-browser/model-browser-component.tsx
@@ -1,6 +1,6 @@
 // this will list the models from the S3 bucket
 // clicking one will display it in a 3D scene
-import { createContext, Suspense, useState } from 'react'
+import { createContext, Suspense, useState, useEffect } from 'react'
 import { Canvas } from "@react-three/fiber"
 import { GizmoHelper, GizmoViewport, Gltf, OrbitControls, PivotControls, Stage } from '@react-three/drei'
 
@@ -44,7 +44,7 @@ export function InteractiveScene(props: any){
             <Gltf 
             castShadow
             position={[0, -0.5, 0]} 
-            src={`models/${props.model}`}
+            src={props.model}
             onClick={modelClicked}
             /> 
         </PivotControls>
@@ -53,7 +53,13 @@ export function InteractiveScene(props: any){
 }
 
 export function ModelBrowser(){
-    const [selectedModel, setSelectedModel] = useState();
+    const [selectedModel, setSelectedModel] = useState<string>();
+
+    useEffect(() => {      
+      const modelUrl =
+      'https://s3.eu-west-1.amazonaws.com/com.verciti.app1/models/Testing/TestingCube.glb?response-content-disposition=inline&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEAAaCWV1LXdlc3QtMSJHMEUCIQDA0dNjFSuhMky67t3fAKCV9oiBIMmxf2oNZSizbotnJQIgUvbsWtuyWRxO5Fv%2BpbiaL3CKJVEr60EQAl%2BBvKNcSoQqwgMI2f%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARAAGgwxMDMyNTk2OTI5MTAiDKTDJLoD9qV%2FJkJgoSqWAxqYnAfP%2FIWaE71zwJVOIKCJIOjP%2BfL08AM7EISjAGE7%2F5NSvCa8o%2FNLXrnhC0XweSk2srmXwdic6oB9P3j2UpdEKLo3%2BZU1YQOphc9hBK10%2F%2Bzl2R8k%2Fo1W9zFdPAyjurxtXghBNy7BS7Z75dT7Mp4EYbYgchpIcsKFJqsC4Y0AUwTJbMXOswtnsfZsbf%2BX7ByEkr21QqyVUcBo2hgrM2Tiumz5w8j9LtM9C10KYC28SqOiTk1Egh34TQzN6XtUCsv4n6woD7pEK1370RLRgkSnclbuNN%2BzW9MttoMyOzLWVAnfgtgyVPXDKD7DR7clhwdtOyGX%2FWC8vjxsonwCrQKcaCrxyx5jvWErisCjO6dlae0e8jbLllhl%2BwC%2FnpGaV09GXo%2F5xrlYTF1Zn%2Fw1qX6ZNtPrNc%2FxLhrnRxw7Fok0dBj9JuR2LsO1MXDk0QE3nxVK6nhms5PP81SFszLRtMUB4n3weKCfq9e3HBr%2F7DR1KVJpW8fxgh402teJSu5wKE0GGyMEyC9o89vOU63llHz300r4APwwnKilwgY63gK4LO63ICy4vFBwsUdvD1%2B%2FZnBVDulbNaECAo9Zy%2FGZAJjS5gjs3zLOZj8uEw2BJCY%2B4%2FshrVGAIol3je9ypeTh%2Bt3N4GJuXVphQeDni7Q504lMKGFPle9f4QbwpLnbgwh40WEOjKybq2s0hU51MXhb7JF6wHwch%2FCE0nBrfG%2F942Ox0b8djZNnOV5J9B7ktR4fT0C9IPK7Fn87Ji8mLajiVSyAnpodhyvr1vJzDjmJ%2FeWZN5m4kOG3S5kPsJYc0xFYYHQ8t8Cs%2FuDiktghgc4je6pTaihzk2bWQb6T5qebnLFI0nO26nkrsif8jyV65jbzHmJo60PVffWktDmlPPBbCnfRdnLjTt8HnSMbd9pq9aHsbGacW49Gg%2BeTWkER3DcZ3mbXjvVJdLkO3DJPw5uDA327kf2NBEgEWCOE63dsAS1AiL2O97uGdaO5TXOHg8mSMV4PYCi3sOgP%2F3LIxQ%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIARQCWB35XFQWYQPUA%2F20250611%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20250611T155111Z&X-Amz-Expires=600&X-Amz-SignedHeaders=host&X-Amz-Signature=a421d715fe9083fbe3cee82f5d1e36d06827350073706422ee638721b5e107e3';
+      setSelectedModel(modelUrl);
+    }, []);
 
     return(
         <>
