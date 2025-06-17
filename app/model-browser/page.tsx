25abe35a16d7f07e8d3b91eb09b0c6136a7883fa
rubenwood <ruben.wood1@gmail.com>
2025-06-17 16:11:08 +0100

    added dependency array to useEffect so we only render once

--- app/model-browser/page.tsx
+++ app/model-browser/page.tsx
@@ -5,21 +5,26 @@ import { User } from '@supabase/supabase-js'
 import { useEffect, useState } from "react";
 
 export default function ModelBrowserPage(){
-    const [user, setUser] = useState<User | null>();
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         const init = async () => {
             const user = await checkUser();
+            
             if (user) { setUser(user); }
+            setLoading(false);
         };
         init();
-    });
-    
+    }, []);
+
+     if (loading) return <p className="p-4">Checking session...</p>;
+    if (!user) return <p>Not logged in</p>;
+
     return (
         <>
-        <div className="header">
-            Model Browser
-        </div>
-        {user ? <ModelBrowser /> : <p>Not logged in</p>}        
+            <div className="header">Model Browser </div>
+            <ModelBrowser />       
         </> 
     )
 }
