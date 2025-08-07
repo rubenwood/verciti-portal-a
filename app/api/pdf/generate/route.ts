e3f933229e8ad6fba42c4fbe7cfa99d218112a2d
rubenwood <ruben.wood1@gmail.com>
2025-08-07 16:32:52 +0100

    abandoned browser based pdf stuff

--- app/api/pdf/generate/route.ts
+++ app/api/pdf/generate/route.ts
@@ -1,4 +1,3 @@
-import chromium from 'chrome-aws-lambda';
 import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import path from 'path';
@@ -10,38 +9,8 @@ export async function POST(req: NextRequest){
         return NextResponse.json({ error: 'Missing HTML input' });
     }
 
-    const browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-    });
+    
 
-    const page = await browser.newPage();
-
-    await page.setContent(html, {
-      waitUntil: 'networkidle0',
-    });
-
-    const pdfBuffer = await page.pdf({
-      format: 'a4',
-      printBackground: true,
-      margin:{
-        top:20,
-        bottom:20,
-        left:20,
-        right:20,
-      }
-    });
-
-    await browser.close();
-
-    return new Response(pdfBuffer, {
-        status: 200,
-        headers: {
-            'Content-Type': 'application/pdf',
-            'Content-Disposition': 'inline; filename="generated.pdf"',
-        }
-    })
+    return null;
 
 }
