4733f10ecd82546a845f15b7c3a881d8a5e64cbe
rubenwood <ruben.wood1@gmail.com>
2025-09-17 16:05:11 +0100

    updated redirect uri

--- app/google-login/page.tsx
+++ app/google-login/page.tsx
@@ -1,16 +1,13 @@
 "use client"
 import { useEffect, useState } from "react";
 
-export default function GoogleLogin(props: any){
-
-
+export default function GoogleLogin(){
 
     return(
         <div>
             <button className="button mt-2">
                 Return to Verciti App
             </button>
-            {props.error && <p className="text-red-500 mt-2">{props.error}</p>}
         </div>
     );
 }
--- app/login/login-component.tsx
+++ app/login/login-component.tsx
@@ -29,7 +29,7 @@ export default function Login(props: any){
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: 'https://qufeoomnqojcinjgnczw.supabase.co/auth/v1/callback'
+                redirectTo: 'https://vertciti-portal.vercel.app/google-login'
             }
         });
         if(error){
