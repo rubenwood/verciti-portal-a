51144084677fc61ad7b46d3d919474ab95507d6d
rubenwood <ruben.wood1@gmail.com>
2025-09-17 16:01:23 +0100

    test

--- app/google-login/page.tsx
+++ app/google-login/page.tsx
@@ -3,21 +3,11 @@ import { useEffect, useState } from "react";
 
 export default function GoogleLogin(props: any){
 
-    useEffect(() => {
-        //const hash = window.location.hash.slice(1); // remove #
-        //const hashParams = new URLSearchParams(hash);
-//
-        //const at = hashParams.get("access_token");
-        //const rt = hashParams.get("refresh_token");
-    });
 
-    const openApp = () =>{
-        window.open('verciti://edtechapp');
-    }
 
     return(
         <div>
-            <button className="button mt-2" onClick={openApp}>
+            <button className="button mt-2">
                 Return to Verciti App
             </button>
             {props.error && <p className="text-red-500 mt-2">{props.error}</p>}
