86beae4c12e44895504f6c2ca8e7f9713cdf75c7
rubenwood <ruben.wood1@gmail.com>
2025-10-15 15:14:51 +0100

    Updated path to get models

--- app/api/aws/list-model-files/route.ts
+++ app/api/aws/list-model-files/route.ts
@@ -11,7 +11,7 @@ const s3 = new S3Client({
 });
 
 const BUCKET = "com.verciti.app1";
-const PREFIX = "public/";
+const PREFIX = "dev/public/models/Testing/";
 
 export async function GET(req: Request) {
   try {
