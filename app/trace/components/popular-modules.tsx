447af5614c41ab41eba056de9fe3ab3c214ef6f6
rubenwood <ruben.wood1@gmail.com>
2026-01-12 15:56:47 +0000

    added a function to get the most played activities by number of users

--- app/trace/components/popular-modules.tsx
+++ app/trace/components/popular-modules.tsx
@@ -9,8 +9,35 @@ export function PopularModulesCard(props: any){
     const [mostUsersModule, setMostUsersModule] = useState<string>("");
     const [mostPlayTime, setMostPlayTimeModule] = useState<string>("");    
 
-    useEffect(() => {
 
+    function allMostFrequent(arr: any[]): any[] {
+        const count = new Map();
+        let maxFreq = 0;
+
+        for (let val of arr) {
+            const freq = (count.get(val) || 0) + 1;
+            count.set(val, freq);
+            if (freq > maxFreq) maxFreq = freq;
+        }
+        return [...count.entries()]
+            .filter(([key, freq]) => freq === maxFreq)
+            .map(([key]) => key);
+    }
+
+    const findMostPopularByUsers = () => {
+        const activities: string[] = [];
+        for (const user of props.data) {
+            for(const activity of user.generic_activity_progress){
+                console.log("Activity:", activity.activity_id);
+                activities.push(activity.activity_id);
+            }
+        }
+        const mostFrequent = allMostFrequent(activities);
+        console.log("Most Frequent Activities by Users:", mostFrequent);
+    }
+
+    useEffect(() => {
+        findMostPopularByUsers();
     }, [props.data]);    
 
     return (
@@ -21,9 +48,9 @@ export function PopularModulesCard(props: any){
                 </CardTitle>
             </CardHeader>
             <CardContent className="flex flex-1 items-center justify-center">
-                <p>Most Played Module (Num plays): {mostPlayedModule}</p><br/>
-                <p>Most Played Module (Num users): {mostUsersModule}</p><br/>
-                <p>Most Played Module (Play time): {mostPlayTime}</p><br/>
+                Most Played Module (Num plays): {mostPlayedModule}<br/>
+                Most Played Module (Num users): {mostUsersModule}<br/>
+                Most Played Module (Play time): {mostPlayTime}<br/>
             </CardContent>
         </Card>
     )
