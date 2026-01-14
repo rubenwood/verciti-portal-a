c72ddf2a012b8f83f3573e18e341170bd1d7faa3
rubenwood <ruben.wood1@gmail.com>
2026-01-14 11:15:17 +0000

    renamed file

--- app/trace/components/popular-modules.tsx
+++ app/trace/components/popular-modules.tsx
@@ -6,19 +6,23 @@ import { formatDuration } from "@/app/db/general/utils";
 export function PopularModulesCard(props: any){
     useEffect(() => {
 
-    }, [props.mostPlayed, props.mostPlayedByUserCount, props.mostPlayedTime]);    
+    }, [props.mostPlayed, props.mostPlayedByUserCount, props.mostPlayedTime]);
+
+    const formatModuleTitles = (titles: string[]) => {
+        return titles.map(title => <><p key={title}>{title}</p><br/></>)
+    }
 
     return (
         <Card className="mb-4 p-4 flex flex-col">
             <CardHeader>
                 <CardTitle className="text-lg font-semibold text-center">
-                    Popular Modules
+                    Most Popular Modules
                 </CardTitle>
             </CardHeader>
             <CardContent className="flex flex-1 items-center justify-center">
-                Most Played Module (Num plays):<br/>{props.mostPlayed?.moduleTitle} - {props.mostPlayed?.playCount}<br/><br/>
-                Most Played Module (Num users):<br/>{JSON.stringify(props.mostPlayedByUserCount.moduleTitles)} - {props.mostPlayedByUserCount.userCount}<br/><br/>
-                Most Played Module (Play time):<br/>{props.mostPlayedTime?.moduleTitle} - {formatDuration(props.mostPlayedTime?.playTime)}<br/><br/>
+                Most Played Module (# plays):<br/>{props.mostPlayed?.moduleTitle} - {props.mostPlayed?.playCount}<br/><br/>
+                Most Played Module (# users):<br/>{props.mostPlayedByUserCount.moduleTitles.join(", ")} - {props.mostPlayedByUserCount.userCount}<br/><br/>
+                Most Played Module (time):<br/>{props.mostPlayedTime?.moduleTitle} - {formatDuration(props.mostPlayedTime?.playTime)}<br/><br/>
             </CardContent>
         </Card>
     )
