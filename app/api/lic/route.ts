665ce4d80d5fff9a4b4d0275d4b9612d471838ba
rubenwood <ruben.wood1@gmail.com>
2026-02-03 14:46:17 +0000

    added code to update content tags for user

--- app/api/lic/route.ts
+++ app/api/lic/route.ts
@@ -13,6 +13,7 @@ export async function POST(req: Request) {
     const request = await req.json();
     console.log("API call to check-email-access with request:", request);
 
+    const userId = request.record.data.id;
     const email = request.record.data.email;
     console.log("email: ", email);
     const suffix = email.split('@')[1];
@@ -26,6 +27,27 @@ export async function POST(req: Request) {
     const emailAddressMatch = await supabaseService.from(orgTableName).select('*').contains(addrColName, [email]);
     console.log("suf match: ", suffixMatch);
     console.log("addr match:", emailAddressMatch);
+
+    if(suffixMatch.data){
+        // add content_tags to acc
+        // using first result for now
+        const contentTags = ["Production"]; // must always have prod
+        contentTags.push(suffixMatch.data[0].content_tags);
+        console.log("content tags: ", contentTags)
+        // update the user account with these content tags
+        const {data, error} = await supabaseService
+        .from('user_profiiles')
+        .update({content_visiblity:contentTags})
+        .eq('id', userId);
+
+        if(error){
+            console.error("Error updating content tags: ", error);
+        }else{
+            console.log("Success updating content tags: ", data);
+        }
+
+        
+    }
 
     const response = {suffMatch:suffixMatch, addrMatch:emailAddressMatch};
     return NextResponse.json({ response });
