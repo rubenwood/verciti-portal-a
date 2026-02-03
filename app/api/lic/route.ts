2094f16ad030c351165eb7eee6632ee4f36a9b8b
rubenwood <ruben.wood1@gmail.com>
2026-02-03 14:30:21 +0000

    using service role

--- app/api/lic/route.ts
+++ app/api/lic/route.ts
@@ -1,14 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { createServerTestClient } from '@/lib/server';
-
-import { supabaseTest } from '@/lib/supabase';
+//import { createServerTestClient } from '@/lib/server';
+//import { supabaseTest } from '@/lib/supabase';
+import { createClient } from '@supabase/supabase-js';
 
 // called by auth webhook
 export async function POST(req: Request) {
+    const supabaseService = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_TEST_URL!,
+        process.env.SUPABASE_SEC_KEY!,
+    )
+
     const request = await req.json();
     console.log("API call to check-email-access with request:", request);
 
     const email = request.record.data.email;
+    console.log("email: ", email);
     const suffix = email.split('@')[1];
     
     const orgTableName = process.env.ORG_TABLE_NAME!;
@@ -16,8 +22,8 @@ export async function POST(req: Request) {
     const addrColName = process.env.ADDR_COL!;
 
     // will need to update this to use live
-    const suffixMatch = await supabaseTest.from(orgTableName).select('*').contains(sufColName, [suffix]);
-    const emailAddressMatch = await supabaseTest.from(orgTableName).select('*').contains(addrColName, [email]);
+    const suffixMatch = await supabaseService.from(orgTableName).select('*').contains(sufColName, [suffix]);
+    const emailAddressMatch = await supabaseService.from(orgTableName).select('*').contains(addrColName, [email]);
     console.log("suf match: ", suffixMatch);
     console.log("addr match:", emailAddressMatch);
 
