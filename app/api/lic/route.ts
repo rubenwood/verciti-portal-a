d612adf388123de2717090ccff85cd2b70f76806
rubenwood <ruben.wood1@gmail.com>
2026-02-03 13:47:17 +0000

    code should now grab email address

--- app/api/lic/route.ts
+++ app/api/lic/route.ts
@@ -8,12 +8,13 @@ export async function POST(req: Request) {
     const request = await req.json();
     console.log("API call to check-email-access with request:", request);
 
-    // const email = request.email;
-    // const suffix = email.split('@')[1];
+    const email = request.record.data.email;
+    const suffix = email.split('@')[1];
 
-    // const suffixMatch = supabaseTest.from('org_access').select('*').contains('email_suffixes', [suffix]);
-    // const emailAddressMatch = supabaseTest.from('org_access').select('*').contains('email_addresses', [email]);
-    // console.log("check for email:", email, "Result:", emailAddressMatch, suffixMatch);
+    // will need to update this to use live
+    const suffixMatch = supabaseTest.from('org_access').select('*').contains('email_suffixes', [suffix]);
+    const emailAddressMatch = supabaseTest.from('org_access').select('*').contains('email_addresses', [email]);
+    console.log("check for email:", email, "Result:", emailAddressMatch, suffixMatch);
 
     const response = "hello";
     return NextResponse.json({ response });
