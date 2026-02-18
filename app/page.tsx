554e4ef14f0478001a006370e2ee2eca8b248d4e
N/A <ruben.wood1@gmail.com>
2026-02-18 10:17:57 +0000

    more logging

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,12 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
+    const hashParams = new URLSearchParams(window.location.hash.substring(1));
+    const searchParams = new URLSearchParams(window.location.search);
+    console.log("URL hash:", window.location.hash);
+    console.log("URL search params:", window.location.search);
+
+
     // TODO: verify this
     const hash = window.location.hash;
     console.log("URL hash:", hash);
