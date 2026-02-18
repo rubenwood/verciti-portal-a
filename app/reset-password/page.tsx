c90ab61bcfd261be542ebd5d01ca8d199ac6cb08
N/A <ruben.wood1@gmail.com>
2026-02-18 11:50:42 +0000

    re-added log

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -11,7 +11,7 @@ export default function ResetPassword() {
 
   useEffect(() => {
     const hash = window.location.hash;
-
+    console.log("URL hash:", hash);
     //TODO: need to switch based off of the account
     // could be an account from test or main
 
