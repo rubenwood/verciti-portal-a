098800b7d92220423c02a11c567d18f8f187e72a
N/A <ruben.wood1@gmail.com>
2026-02-18 10:25:40 +0000

    public route logic

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -37,13 +37,21 @@ export async function updateSession(request: NextRequest) {
 
   const user = data?.claims
 
-  if (
-    !user &&
-    !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/api/') &&
-    !request.nextUrl.pathname.startsWith('/reset-password') &&
-    !request.nextUrl.pathname.startsWith('/update-password')
-  ) {
+  const publicRoutes = [
+    '/',
+    '/login',
+    '/reset-password',
+    '/update-password',
+  ]
+
+  const isPublicRoute =
+    publicRoutes.some(route =>
+      request.nextUrl.pathname === route || request.nextUrl.pathname.startsWith(route + '/')
+    ) ||
+    request.nextUrl.pathname.startsWith('/api/')
+
+
+  if (!user && !isPublicRoute) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
     url.pathname = '/login'
