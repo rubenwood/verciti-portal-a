1820b4fa7f442ddd457aab6528ea433df2bf3735
N/A <ruben.wood1@gmail.com>
2026-04-07 11:09:01 +0100

    rollback

--- app/apple-login/live/page.tsx
+++ app/apple-login/live/page.tsx
@@ -1,9 +1,8 @@
 import AppleLogin from "../apple-login";
-import { supabaseMain } from '@/lib/supabase'
 
 
 export default function AppleLoginLive(){
     return(
-        <AppleLogin client={supabaseMain} />
+        <AppleLogin client="live"/>
     )
 }
--- app/apple-login/test/page.tsx
+++ app/apple-login/test/page.tsx
@@ -1,8 +1,8 @@
 import AppleLogin from "../apple-login";
-import { supabaseTest } from '@/lib/supabase'
+
 
 export default function AppleLoginTest(){
     return(
-        <AppleLogin client={supabaseTest} />
+        <AppleLogin client="test"/>
     )
 }
