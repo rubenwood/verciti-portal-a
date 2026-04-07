e8c3d7a19e77726bf3f8c313433c22ba4d0f91eb
N/A <ruben.wood1@gmail.com>
2026-04-07 10:10:40 +0100

    added storage key to supabase clients

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,9 +3,15 @@ import { createClient } from '@supabase/supabase-js'
 export const supabaseMain = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: { storageKey: "sb-live" }
+  }
 )
 
 export const supabaseTest = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_TEST_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_TEST_ANON_KEY!,
+  {
+    auth: { storageKey: "sb-test" }
+  }
 )
