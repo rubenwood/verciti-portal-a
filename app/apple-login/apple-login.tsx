1a7808415afe90a39c0305930c50498cce0abce5
N/A <ruben.wood1@gmail.com>
2026-04-07 13:39:24 +0100

    added another button for ios

--- app/apple-login/apple-login.tsx
+++ app/apple-login/apple-login.tsx
@@ -72,7 +72,10 @@ export default function AppleLogin(props: any){
                     <p>If you are not automatically logged in please click the button below.</p>
                     <Button onClick={login} className="z-50 relative">
                         Sign in with Apple
-                    </Button>            
+                    </Button>
+                    <button onClick={openApp} className="z-50 relative">
+                        Sign in with Apple
+                    </button>
                 </>                
             ) : (
                 <>
