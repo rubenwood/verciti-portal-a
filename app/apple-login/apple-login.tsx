1cf156b168bb71f067624748027997529623675e
N/A <ruben.wood1@gmail.com>
2026-04-07 14:19:26 +0100

    ios button testing

--- app/apple-login/apple-login.tsx
+++ app/apple-login/apple-login.tsx
@@ -5,60 +5,6 @@ import { supabaseMain, supabaseTest } from '@/lib/supabase'
 
 
 export default function AppleLogin(props: any){
-    const [error, setError] = useState('');
-    const [deeplink, setDeeplink] = useState<string | null>(null);
-
-    useEffect(() => {
-        if (window.location.hash && !sessionStorage.getItem("oauth_reloaded")) {
-            sessionStorage.setItem("oauth_reloaded", "true");
-            window.location.reload();
-            return;
-        }
-
-        const hash = window.location.hash;
-
-        if (!hash) return;
-
-        const params = new URLSearchParams(hash.replace("#", ""));
-
-        const accessToken = params.get("access_token");
-        const refreshToken = params.get("refresh_token");
-        console.log('Apple Login tokens from URL:', { accessToken, refreshToken });
-        if(accessToken == null) return;
-
-        const atStr = `&at=${encodeURIComponent(accessToken)}`;
-        const rtStr = refreshToken ? `&rt=${encodeURIComponent(refreshToken)}` : "";
-
-        if (accessToken) {
-            const newDeeplink = `verciti://app?alogin${atStr}${rtStr}`;
-            localStorage.setItem("verciti_deeplink", newDeeplink);
-            setDeeplink(newDeeplink);
-            //window.location.href = newDeeplink;
-        }
-    }, []);
-
-    const login = async () => {
-        console.log('Initiating Apple Login for client:', props.client);
-        const client = props.client === "live" ? supabaseMain : supabaseTest;
-        const { data, error } = await client.auth.signInWithOAuth({
-            provider: "apple",
-            options: {
-                redirectTo: `https://vertciti-portal.vercel.app/apple-login/${props.client}`
-            }
-        });
-
-        if(error){
-            setError(error.message);
-            console.error('Apple Login error:', error.message);
-        } else {
-            console.log('Apple Login initiated:', data);
-        }
-    };
-
-    const openApp = () =>{
-        if(deeplink == null) return;
-        window.location.href = deeplink;
-    }
 
     return(
         <>
@@ -74,31 +20,6 @@ export default function AppleLogin(props: any){
                 >
                 TEST
             </button>
-            <div className="grid grid-cols-1 gap-20 mt-20 p-5 items-center justify-items-center">
-                {deeplink == null ? (
-                    <>
-                        <p>If you are not automatically logged in please click the button below.</p>
-                        <Button onClick={login} className="z-50 relative">
-                            Sign in with Apple
-                        </Button>
-                        
-                        <button onClick={login} className="z-50 relative">
-                            Sign in with Apple
-                        </button>
-                    </>                
-                ) : (
-                    <>
-                        <p className="text-center">If you are not redirected automatically, please click the button below to return to the Verciti App.</p>
-                        <Button onClick={openApp} className="z-50 relative">
-                            Open App
-                        </Button>
-
-                        <button onClick={openApp} className="z-50 relative">
-                            Sign in with Apple
-                        </button>
-                    </>
-                )}
-            </div>
         </>
     )
 }
