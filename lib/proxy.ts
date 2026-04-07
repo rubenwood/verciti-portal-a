b94183df6c8c16ba17e2a0f0833449891044aa13
N/A <ruben.wood1@gmail.com>
2026-04-07 11:22:36 +0100

    better handling of public routes

--- lib/proxy.ts
+++ lib/proxy.ts
@@ -51,6 +51,10 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname.startsWith('/api/')
 
 
+  if (isPublicRoute) {
+    return NextResponse.next({ request })
+  }
+
   if (!user && !isPublicRoute) {
     // no user, respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
