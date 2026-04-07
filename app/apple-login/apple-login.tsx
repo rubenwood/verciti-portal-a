3444ae150d76f76c3c809e2e0c4aff03e5985d2a
N/A <ruben.wood1@gmail.com>
2026-04-07 14:19:46 +0100

    iOS button test

--- app/apple-login/apple-login.tsx
+++ app/apple-login/apple-login.tsx
@@ -1,8 +1,3 @@
-"use client"
-import { useEffect, useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { supabaseMain, supabaseTest } from '@/lib/supabase'
-
 
 export default function AppleLogin(props: any){
 
