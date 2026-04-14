3caaea90b17967c847eaf15b575323134892c5a9
rubenwood <ruben.wood1@gmail.com>
2026-04-14 14:14:45 +0100

    simplified generate pdf

--- app/api/certificate/certificate.ts
+++ app/api/certificate/certificate.ts
@@ -1,41 +1,23 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { getUserFolder } from "../../db/general/utils";
-import crypto from "crypto";
 
 export async function generateCertificate(client: SupabaseClient, certificateData: any) {
-    //const folder = getUserFolder(userId, email);
+    certificateData.templateId = "89544C0C-29A3-4BA7-96FA-D8093244C847";
+    const pdfResp = await generatePDF(certificateData);
 
-    const resp = await fetch("https://api.pdfmonkey.io/api/v1/documents", {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${process.env.PDF_MONKEY_KEY}`,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            document: {
-                document_template_id: "89544C0C-29A3-4BA7-96FA-D8093244C847",
-                payload: {
-                    name: certificateData.name,
-                    message: certificateData.message,
-                    date: certificateData.date,
-                },
-            },
-        }),
-    });
-
-    if (!resp.ok) {
-        const errorData = await resp.json();
+    if (!pdfResp.ok) {
+        const errorData = await pdfResp.json();
         throw new Error(`PDF generation failed: ${errorData.error}`);
     }
 
-    const respJson = await resp.json();
+    const respJson = await pdfResp.json();
     console.log("PDF Monkey response:", respJson), "\n";
 
-    // hash of uid
-    const hash = crypto.createHash('sha256').update(certificateData.uid).digest('hex');
+    const hash = getUserFolder(certificateData.uid);
     //const path = await uploadToStorage(client, hash, null);
 
     // need to also send an email with this certificate
+    // ensure they are scheduled and rate limited properly
 
     const response = {
         folder: hash,
@@ -45,8 +27,30 @@ export async function generateCertificate(client: SupabaseClient, certificateDat
     return response;
 }
 
-async function uploadToStorage(client: SupabaseClient, uid: string, fileBuffer: Buffer) {
-    const filePath = `dev/public/certificates/${uid}/certificate-${Date.now()}.pdf`;
+async function generatePDF(certificateData: any) {
+    const resp = await fetch("https://api.pdfmonkey.io/api/v1/documents", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${process.env.PDF_MONKEY_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            document: {
+                document_template_id: certificateData.templateId,
+                payload: {
+                    name: certificateData.name,
+                    message: certificateData.message,
+                    date: certificateData.date,
+                },
+            },
+        }),
+    });
+
+    return resp;
+}
+
+async function uploadToStorage(client: SupabaseClient, folder: string, fileBuffer: Buffer) {
+    const filePath = `dev/public/certificates/${folder}/certificate-${Date.now()}.pdf`;
 
     const { error } = await client.storage
         .from("certificates")
