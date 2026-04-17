6bcb524ea63f0e1f2d2ddcefbc2ffe557b84bd81
N/A <ruben.wood1@gmail.com>
2026-04-17 11:36:23 +0100

    refined Most Popular card (rows)

--- app/analytics/components/cards/popular-modules-card.tsx
+++ app/analytics/components/cards/popular-modules-card.tsx
@@ -1,59 +1,75 @@
 "use client"
-import { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatDuration } from "@/app/db/general/utils";
 import { ChevronDown, ChevronRight, Clock, Play, User } from "lucide-react";
 import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@/components/ui/table";
+
+interface MostPopularRowProps {
+    title: ReactNode | string;
+    value: ReactNode | string;
+    count: ReactNode | number;
+}
+
+function MostPopularRow(props: MostPopularRowProps) {
+    return (
+        <TableRow className="border-1">
+            <TableCell>
+                {props.title}{/*Most Played <Play className="size-4" />*/}
+            </TableCell>
+            <TableCell>
+                {props?.value || ""}
+            </TableCell>
+            <TableCell>
+                {props?.count || 0} 
+            </TableCell>
+        </TableRow>);
+}
 
 export function PopularModulesCard(props: any) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-      <Card className="mb-2">
-        <CollapsibleTrigger asChild>
-            <CardHeader className="cursor-pointer flex flex-row items-center justify-between p-4">
-                <CardTitle className="text-lg font-semibold">
-                    Most Popular Modules
-                </CardTitle>
-                {isOpen ? (
-                    <ChevronDown className="h-4 w-4" />
-                ) : (
-                    <ChevronRight className="h-4 w-4" />
-                )}
-            </CardHeader>
-        </CollapsibleTrigger>
-        <CollapsibleContent>
-            <CardContent className="pt-0">
-                <div className="grid grid-cols-3 border rounded-sm w-full text-sm">                    
-                    <p className="border-1 p-2 flex items-center gap-1">
-                        Most Played <Play className="size-4" />
-                    </p>
-                    <p className="border-1 p-2">{props.mostPlayed?.moduleTitle}</p>
-                    <p className="border-1 p-2">{props.mostPlayed?.playCount}</p>
-
-                    <p className="border-1 p-2 flex items-center gap-1">
-                        Most Users <User className="size-4" />
-                    </p>
-                    <p className="border-1 p-2">
-                        {props.mostPlayedByUserCount?.moduleTitles?.join(", ")}
-                    </p>
-                    <p className="p-2">
-                    {props.mostPlayedByUserCount?.userCount}
-                    </p>
-
-                    <p className="border-1 p-2 flex items-center gap-1">
-                        Longest Played <Clock className="size-4" />
-                    </p>
-                    <p className="border-1 p-2">{props.mostPlayedTime?.moduleTitle}</p>
-                    <p className="border-1 p-2">
-                        {formatDuration(props.mostPlayedTime?.playTime)}
-                    </p>
-                </div>
-            </CardContent>
-        </CollapsibleContent>
-
-      </Card>
+        <Card className="mb-2">
+            <CollapsibleTrigger asChild>
+                <CardHeader className="cursor-pointer flex flex-row items-center justify-between p-4">
+                    <CardTitle className="text-lg font-semibold">
+                        Most Popular Modules
+                    </CardTitle>
+                    {isOpen ? (
+                        <ChevronDown className="h-4 w-4" />
+                    ) : (
+                        <ChevronRight className="h-4 w-4" />
+                    )}
+                </CardHeader>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+                    <Table className="w-full border-1 p-2">
+                        <TableBody className="border-1">
+                            <MostPopularRow 
+                                title={<>Most Played<Play className="size-4" /></>}
+                                value={props.mostPlayed?.moduleTitle} 
+                                count={props.mostPlayed?.playCount} 
+                            />
+                            <MostPopularRow 
+                                title={<>Most Users <User className="size-4" /></>} 
+                                value={props.mostPlayedByUserCount?.moduleTitles?.map((title: string) => (
+                                    <>
+                                        <span key={`most-played--${title}`}>{title}</span><br/><br/>
+                                    </>
+                                ))} 
+                                count={props.mostPlayedByUserCount?.userCount} />
+                            <MostPopularRow 
+                                title={<>Longest Played <Clock className="size-4" /></>}
+                                value={props.mostPlayed?.moduleTitle} 
+                                count={formatDuration(props.mostPlayedTime?.playTime)} 
+                            />
+                        </TableBody>
+                    </Table>
+            </CollapsibleContent>
+        </Card>
     </Collapsible>
   );
 }
