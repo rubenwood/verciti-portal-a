d95438ff6f0a498d5d3183bb4617edf3d0327f64
N/A <ruben.wood1@gmail.com>
2026-06-26 10:48:00 +0100

    added comment

--- app/api/stripe/checkout-completed/route.ts
+++ app/api/stripe/checkout-completed/route.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 import { createClient } from "@supabase/supabase-js";
 
+//apiVersion: "2026-06-24.dahlia",
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2026-06-24.dahlia",
 });
