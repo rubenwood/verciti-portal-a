67a06edde7f5b3ac401842676e5d86f8463f0927
N/A <ruben.wood1@gmail.com>
2026-07-02 15:47:16 +0100

    updated next config for ffmpeg support

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 // set tubropack to false
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["@ffmpeg-installer/ffmpeg", "fluent-ffmpeg"],
 };
 
 export default nextConfig;
